3042b443779ec1b92fed9af6fae477ef2e56af39
Akshat <akku.gupta20801@gmail.com>
2021-05-11 15:29:23 +0530

    added lw-lw, sw-lw, sw-sw redundancy refactored structure

---
+++ instParser.hpp
@@ -0,0 +1,226 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstring>
+#include "helper.hpp"
+
+#include <unordered_map>
+using namespace std;
+
+#define ll long long int
+
+vector<ll> inst_size;
+vector<vector<pair<string, ll>>> inst;
+vector<vector<Instruction>> instruction_list;
+vector<vector<string>> oinst;
+vector<unordered_map<string, int>> labels; 
+
+//Function to parse Current Instruction and store it to Instruction_list
+void parse(pair<string, long long> instru, int core_num)
+{
+    string line = instru.first;
+    long long num = instru.second;
+
+    //defining variables for Struct Instruction
+    string keyword;
+    vector<string> variables;
+    vector<int> arguments;
+
+    // find the operator
+    size_t first = line.find(" ");
+    if (first == string::npos)
+    {
+        first = line.find("\t");
+    }
+    else if (line.find("\t") != string::npos)
+    {
+        size_t first = min(first, line.find("\t"));
+    }
+    if (first == string::npos)
+    {
+
+        throw runtime_error("Syntax Error at line " + to_string(num) + ": " + line);
+    }
+    keyword = line.substr(0, first);
+
+    // remaining string
+    string remain = trimmed(line.substr(first));
+
+    //vector to store all arguments provided in line
+    vector<string> remains;
+    size_t end = remain.find(",");
+    while (end != std::string::npos)
+    {
+        remains.push_back(trimmed(remain.substr(0, end)));
+
+        remain = remain.substr(end + 1);
+        end = remain.find(",");
+    }
+    remains.push_back(trimmed(remain));
+
+    //Checking syntax if Instruction is starting with "add","sub","mul","slt" and adding it to instruction_list
+    // Throws errors if anything is syntactically incorrect
+    if (keyword.compare("add") == 0 || keyword.compare("sub") == 0 || keyword.compare("mul") == 0 || keyword.compare("slt") == 0)
+    {
+        if (remains.size() != 3)
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        if (registers.find(remains[0]) == registers.end() || registers.find(remains[1]) == registers.end() || registers.find(remains[2]) == registers.end())
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        variables.push_back(remains[0]);
+        variables.push_back(remains[1]);
+        variables.push_back(remains[2]);
+        Instruction i = {keyword, variables, arguments, num};
+        instruction_list[core_num].push_back(i);
+        return;
+    }
+
+    //Checking syntax if Instruction is starting with "j" and adding it to instruction_list
+    // Throws errors if anything is syntactically incorrect
+    else if (keyword == "j")
+    {
+
+        if (remains.size() != 1)
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        if (labels[core_num].find(remains[0]) == labels[core_num].end())
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        variables.push_back(remains[0]);
+        Instruction i = {keyword, variables, arguments, num};
+        instruction_list[core_num].push_back(i);
+        return;
+    }
+
+    //Checking syntax if Instruction is starting with "beq","bne" and adding it to instruction_list
+    else if (keyword == "beq" || keyword == "bne")
+    {
+        if (remains.size() != 3)
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        if (registers.find(remains[0]) == registers.end() || registers.find(remains[1]) == registers.end() || labels[core_num].find(remains[2]) == labels[core_num].end())
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        variables.push_back(remains[0]);
+        variables.push_back(remains[1]);
+        variables.push_back(remains[2]);
+        Instruction i = {keyword, variables, arguments, num};
+        instruction_list[core_num].push_back(i);
+        return;
+    }
+
+    //Checking syntax if Instruction is starting with "lw","sw" and adding it to instruction_list
+    // Throws errors if anything is syntactically incorrect
+    else if (keyword == "lw" || keyword == "sw")
+    {
+
+        if (remains.size() != 2)
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        if (registers.find(remains[0]) == registers.end())
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        variables.push_back(remains[0]);
+        if (remains[1].find("(") != string::npos)
+        {
+            int ind = remains[1].find("(");
+            string reg = trimmed(remains[1].substr(ind));
+            if (reg[reg.length() - 1] == ')' && registers.find(reg.substr(1, reg.length() - 2)) != registers.end())
+            {
+                variables.push_back(reg.substr(1, reg.length() - 2));
+            }
+            else
+            {
+                throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+            }
+            string offset = trimmed(remains[1].substr(0, ind));
+            if (offset == "")
+            {
+                arguments.push_back(0);
+            }
+            else
+            {
+                try
+                {
+                    arguments.push_back(stoi(offset));
+                }
+                catch (exception)
+                {
+                    throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+                }
+            }
+            Instruction i = {keyword, variables, arguments, num};
+            instruction_list[core_num].push_back(i);
+            return;
+        }
+        else
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+    }
+
+    //Checking syntax if Instruction is starting with "addi" and adding it to instruction_list
+    // Throws errors if anything is syntactically incorrect
+    else if (keyword == "addi")
+    {
+
+        if (remains.size() != 3)
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+
+        if (registers.find(remains[0]) == registers.end() || registers.find(remains[1]) == registers.end())
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        variables.push_back(remains[0]);
+        variables.push_back(remains[1]);
+        try
+        {
+
+            arguments.push_back(stoi(remains[2]));
+        }
+        catch (exception)
+        {
+
+            throw runtime_error("Syntax Error in \"" + keyword + "\" instruction at line " + to_string(num) + ": " + line);
+        }
+        Instruction i = {keyword, variables, arguments, num};
+        instruction_list[core_num].push_back(i);
+        return;
+    }
+
+    //Throw Error when given Instruction doesn't match with any operations mention above
+    else
+    {
+
+        throw runtime_error("syntax error in line " + to_string(num) + ": " + line);
+    }
+    return;
+}
+
+
--- reqManager.hpp
+++ reqManager.hpp
@@ -6,27 +6,57 @@
 using namespace std;
 
 const int max_queue_size = 64;
+
+int32_t **memory = NULL;
+
+int32_t *buffer_row = NULL;
+int write_val = -1;
+bool forwarding = false;
+bool storeRedun = false;
+bool loadRedun = false;
 int put_back_end = -1, put_back_start = -1, row_access_end = -1, row_access_start = -1, column_access_start = -1, column_access_end = -1, put_back_row = -1, request_issue_cycle= -1;
 struct d_request{
-    int addr = -1, access_row = -1, access_column =-1, op = -1, data_bus = -1,core_num =0;
+    int addr = -1, access_row = -1, access_column =-1, op = -1, data_bus = -1,core_num =-1;
     string waiting_reg;
     bool issue_msg = false;
     
 };
+
+struct position{
+    bool check = false;
+    int queue;
+    int ind;
+};
+
+struct d_request null_req = {-1,-1,-1,-1,-1,-1,"",false};
 int curr_queue = -1;            // curr queue
 bool in_buffer = false;
 d_request request_queue[8][max_queue_size];     //dram request queues
 int queue_sizes[8];                      //sizes of dram request queues
 int assigned_rows[1024];                //row - queue mapping
 int first_req[8];                        //chronologically first req of 
-
+int assigned_queues[8];
+position **loadReqs = NULL;
 int num_assigned = 0;
-void erase_req(int ind);
+
+void switchQueue(int buffer, int cycle);
 void print_reqs();
 
 
+
 void assign_cycle_values(int last_buffer, int last_cycle){
-    d_request req = request_queue[curr_queue][first_req[curr_queue]];
+    d_request req;
+    while(queue_sizes[curr_queue] != 0){
+        req = request_queue[curr_queue][first_req[curr_queue]];
+        if(req.addr != -1)
+            break;
+        first_req[curr_queue] = (first_req[curr_queue] + 1) % max_queue_size;
+        queue_sizes[curr_queue]--;
+    }
+
+    if(queue_sizes[curr_queue] == 0){
+        switchQueue(last_buffer, last_cycle);
+    }
     request_issue_cycle = last_cycle + 1;
     if(last_buffer == req.access_row){
         put_back_end = -1;
@@ -59,20 +89,13 @@ void assign_cycle_values(int last_buffer, int last_cycle){
 
 }
 
-bool assign_queue(int row){
-    if(assigned_rows[row] != -1){
-        if(queue_sizes[assigned_rows[row]] == max_queue_size){
-            return false;
-        }
-        return true;
-    }
-    for(int i = 0; i<8; i++){
+void removeRedundAssigned(){
+    for(int i =0; i < 8; i++){
         if(queue_sizes[i] == 0){
-            assigned_rows[row] = i;
-            return true;
+            assigned_rows[assigned_queues[i]] = -1;
+            assigned_queues[i] = -1;
         }
     }
-    return false;
 }
 
 void add_req(Instruction I, int addr, int last_buffer, int last_cycle,int core_num , int data_bus_value = -1 ){
@@ -96,82 +119,68 @@ void add_req(Instruction I, int addr, int last_buffer, int last_cycle,int core_n
         in_buffer = true;
         curr_queue = assigned_rows[req.access_row];
         request_queue[curr_queue][queue_sizes[curr_queue]+first_req[curr_queue] % max_queue_size] = req;
-        assign_cycle_values(last_buffer, last_cycle);
         queue_sizes[assigned_rows[req.access_row]] += 1;
+        assign_cycle_values(last_buffer, last_cycle);
         return;
     }
-    // int skip_ind = -1;
-    // int skip_addr = -1;
-    // for(int i=request_queue.size() -1; i>0; i--){
-    //     if(req.op == 0){
-    //         if(request_queue[i].op == 1 && request_queue[i].addr == addr){
-    //             break;
-    //         }
-    //         if(request_queue[i].op == 0 && request_queue[i].addr == addr){
-    //             erase_req(i);
-    //             i = min((int)request_queue.size(), i+1);
 
-    //         }
-    //     }
-    //     if(req.op == 1){
-    //         if(request_queue[i].op == 1 && request_queue[i].waiting_reg == req.waiting_reg && request_queue[i].core_num == req.core_num){
-    //             skip_addr = request_queue[i].addr;
-    //             erase_req(i);
-    //             skip_ind = i;
-    //             break;
-    //         }
-    //     }
-    // }
-
-    // if(skip_ind != -1){
-    //     int eligible_ind = -1;
-    //     int skip_row = getRow(skip_addr/4);
-    //     for(int i = skip_ind; i<request_queue.size(); i++){
-    //         if(request_queue[i].access_row != skip_row){
-    //             break;
-    //         }
-    //         if(request_queue[i].op == 1 && request_queue[i].addr == skip_addr){
-    //             break;
-    //         }
-    //         if(request_queue[i].op == 0 && request_queue[i].addr == skip_addr){
-    //             eligible_ind = i;
-    //             break;
-    //         }
-
-    //     }
-    //     int k=0;
-    //     for(int i = eligible_ind-1; i>0; i--){
-    //         if(request_queue[i].op == 1 && request_queue[i].addr == skip_addr){
-    //             break;
-    //         }
-    //         if(request_queue[i].op == 0 && request_queue[i].addr == skip_addr){
-    //             erase_req(i);
-    //             k++;
-    //             i = min(eligible_ind -k, i+1);
-
-    //         }
-    //     }
-    // }
-
-    // int index = -1;
-    // for(int i = request_queue.size() -1; i >= 0; i--){
-    //     if(request_queue[i].access_row == req.access_row){
-    //         index = i+ 1;
-    //         break;
-    //     }
-    // }
-    // if(index == -1 || index == request_queue.size()){
-    //     request_queue.push_back(req);
-    // }
-    // else{
-    //     request_queue.insert(request_queue.begin() + index, req);
-    // }
     int q = assigned_rows[req.access_row];
+    for(int j = 0; j<queue_sizes[q]; j++){
+        d_request j_req = request_queue[q][(j+ first_req[q]) % max_queue_size];
+        if(j_req.op == 0 && req.addr == j_req.addr){
+            if(req.op == 1){
+                forwarding = true;
+                write_val = j_req.data_bus;
+                return;
+            }
+            if(req.op==0){
+                request_queue[q][(j+ first_req[q]) % max_queue_size] = req;
+                storeRedun = true;
+                return;
+            }
+
+        }
+    }
+
+    if(req.op == 1){
+        position pos = loadReqs[core_num][registers[req.waiting_reg]];
+        if(pos.check){
+            request_queue[pos.queue][pos.ind] = null_req;
+            loadRedun = true;
+        }
+        loadReqs[core_num][registers[req.waiting_reg]] ={true, q, (queue_sizes[q]+first_req[q]) % max_queue_size};    
+    }
     request_queue[q][(queue_sizes[q]+first_req[q]) % max_queue_size] = req;
     queue_sizes[q] += 1;
     return;
 
     
+}
+
+void switchQueue(int buffer, int cycle){
+    bool found = false;
+    for(int i =0; i<8; i++){
+        if(queue_sizes[(i+curr_queue) % 8] != 0){
+            curr_queue = (i+curr_queue) % 8;
+            found = true;
+            break;
+        }
+    }
+    if(found)
+        assign_cycle_values(buffer, cycle);
+    else
+        in_buffer = false;
+}
+
+void issue_next_request(int buffer, int cycle){
+    if(queue_sizes[curr_queue] != 0){
+        assign_cycle_values(buffer, cycle);
+    }
+    else{
+        assigned_rows[assigned_queues[curr_queue]] = -1;
+        assigned_queues[curr_queue] = -1;
+        switchQueue(buffer, cycle);
+    }
 }
 
 bool is_safe(Instruction temp, int core_num){
@@ -205,24 +214,21 @@ bool is_lw_safe(Instruction temp ,int core_num){
     return true;
 }
 
-void issue_next_request(int buffer, int cycle){
-    if(queue_sizes[curr_queue] != 0){
-        assign_cycle_values(buffer, cycle);
-    }
-    else{
-        bool found = false;
-        for(int i =0; i<8; i++){
-            if(queue_sizes[(i+curr_queue) % 8] != 0){
-                curr_queue = (i+curr_queue) % 8;
-                found = true;
-                break;
-            }
+bool assign_queue(int row){
+    if(assigned_rows[row] != -1){
+        if(queue_sizes[assigned_rows[row]] == max_queue_size){
+            return false;
         }
-        if(found)
-            assign_cycle_values(buffer, cycle);
-        else
-            in_buffer = false;
+        return true;
     }
+    for(int i = 0; i<8; i++){
+        if(queue_sizes[i] == 0){
+            assigned_rows[row] = i;
+            assigned_queues[i] = row;
+            return true;
+        }
+    }
+    return false;
 }
 
 int reg_blocked(string reg, int core){
@@ -240,33 +246,6 @@ int reg_blocked(string reg, int core){
 }
 
 
-// void reord_reg(string reg, int core){
-//     int index = -1;
-//     int row = request_queue[0].access_row;
-//     for(int i =0; i<request_queue.size(); i++){
-//         if(request_queue[i].op ==1 && request_queue[i].waiting_reg == reg && request_queue[i].core_num == core){
-//             index = i;
-//             break;
-//         }
-//     }
-//     if(index!=-1){
-//         d_request origin_req = request_queue[0];
-//         int origin_ind =0;
-//         int addr = request_queue[index].addr;
-//         while(index>=origin_ind){
-//             if(request_queue[index].addr == addr){
-//                 d_request req = request_queue[index];
-//                 erase_req(index);
-//                 request_queue.insert(request_queue.begin()+ 0, req);
-//                 origin_ind++;
-//             }
-//             else{
-//                 index--;
-//             }
-//         }
-//     }
-// }
-
 void print_reqs(){
     cout<< "addresses are:";
     for(int i = 0; i<8; i++){
@@ -276,4 +255,13 @@ void print_reqs(){
         }
     }
     cout<<endl;
+}
+
+
+int total_queue_size(){
+    int sum = 0;
+    for(int i=0;i<8;i++){
+        sum+=queue_sizes[i];
+    }
+    return sum;
 }
